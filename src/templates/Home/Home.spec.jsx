e84c48760346ade12522c240328dc4b4ad97d646
BrianRonin <01100010v@gmail.com>
2022-08-05 16:05:09 -0300

    aprendendo testes

--- src/templates/Home/Home.spec.jsx
+++ src/templates/Home/Home.spec.jsx
@@ -1,5 +1,68 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { Home } from '.'
+
+const handlers = [
+  // eu posso usar * pra dizer qualquer requisição desta url pra frente tipo /posts & /photos
+  rest.get('https://jsonplaceholder.typicode.com/posts', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: 'titulo 1',
+          body: 'o rato 1',
+        },
+        {
+          userId: 2,
+          id: 2,
+          title: 'titulo 2',
+          body: 'o rato 2',
+        },
+      ]),
+    )
+  }),
+  rest.get('https://jsonplaceholder.typicode.com/photos', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          albumId: 1,
+          id: 1,
+          title: 'feasfeas',
+          url: 'https://via.placeholder.com/600/92c952',
+          thumbnailUrl: 'https://via.placeholder.com/150/92c952',
+        },
+        {
+          albumId: 2,
+          id: 2,
+          title: 'gsgsegsegsegsegsegsegsgsgsg',
+          url: 'https://via.placeholder.com/600/92c952',
+          thumbnailUrl: 'https://via.placeholder.com/150/92c952',
+        },
+      ]),
+    )
+  }),
+]
+
+const server = setupServer(...handlers)
+
 describe('<Home />', () => {
-  it('is a dumy test', () => {
-    expect(1).toBe(1)
+  beforeAll(() => {
+    server.listen()
+  })
+  afterEach(() => {
+    server.resetHandlers()
+  })
+  afterAll(() => {
+    server.close()
+  })
+
+  it('should render search, posts and load more', async () => {
+    render(<Home />)
+    await waitFor(() => screen.getByRole('heading', 'titulo 1'))
+    console.log(screen.debug())
   })
 })
